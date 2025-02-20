7eff415b2461fcba45fbe4956996f1343b0d1726
Trần Trung Kiên <trantrungkieen2809@gmail.com>
2025-02-20 19:14:21 +0700

    done thay đổi giao diện form login

---
+++ QuanLyNhaHang/Dangki.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace QuanLyNhaHang
+{
+    partial class FormDangKi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtUserNameDangKi = new System.Windows.Forms.TextBox();
+            this.lblUserNameDangKi = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtDisplayNameDangKi = new System.Windows.Forms.TextBox();
+            this.lblDisplayNameDangKi = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtPassWordDangKi = new System.Windows.Forms.TextBox();
+            this.lblPassWordDangKi = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.txtRePassDangKi = new System.Windows.Forms.TextBox();
+            this.lblRePassDangKi = new System.Windows.Forms.Label();
+            this.btnDangKi = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtUserNameDangKi);
+            this.panel2.Controls.Add(this.lblUserNameDangKi);
+            this.panel2.Location = new System.Drawing.Point(70, 21);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(550, 55);
+            this.panel2.TabIndex = 1;
+            // 
+            // txtUserNameDangKi
+            // 
+            this.txtUserNameDangKi.Location = new System.Drawing.Point(215, 19);
+            this.txtUserNameDangKi.Name = "txtUserNameDangKi";
+            this.txtUserNameDangKi.Size = new System.Drawing.Size(310, 22);
+            this.txtUserNameDangKi.TabIndex = 2;
+            // 
+            // lblUserNameDangKi
+            // 
+            this.lblUserNameDangKi.AutoSize = true;
+            this.lblUserNameDangKi.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserNameDangKi.Location = new System.Drawing.Point(20, 19);
+            this.lblUserNameDangKi.Name = "lblUserNameDangKi";
+            this.lblUserNameDangKi.Size = new System.Drawing.Size(165, 24);
+            this.lblUserNameDangKi.TabIndex = 0;
+            this.lblUserNameDangKi.Text = "Tên đăng nhập :";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtDisplayNameDangKi);
+            this.panel1.Controls.Add(this.lblDisplayNameDangKi);
+            this.panel1.Location = new System.Drawing.Point(70, 82);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(550, 55);
+            this.panel1.TabIndex = 3;
+            // 
+            // txtDisplayNameDangKi
+            // 
+            this.txtDisplayNameDangKi.Location = new System.Drawing.Point(215, 19);
+            this.txtDisplayNameDangKi.Name = "txtDisplayNameDangKi";
+            this.txtDisplayNameDangKi.Size = new System.Drawing.Size(310, 22);
+            this.txtDisplayNameDangKi.TabIndex = 2;
+            // 
+            // lblDisplayNameDangKi
+            // 
+            this.lblDisplayNameDangKi.AutoSize = true;
+            this.lblDisplayNameDangKi.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplayNameDangKi.Location = new System.Drawing.Point(20, 19);
+            this.lblDisplayNameDangKi.Name = "lblDisplayNameDangKi";
+            this.lblDisplayNameDangKi.Size = new System.Drawing.Size(135, 24);
+            this.lblDisplayNameDangKi.TabIndex = 0;
+            this.lblDisplayNameDangKi.Text = "Tên hiển thị :";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.txtPassWordDangKi);
+            this.panel3.Controls.Add(this.lblPassWordDangKi);
+            this.panel3.Location = new System.Drawing.Point(70, 143);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(550, 55);
+            this.panel3.TabIndex = 3;
+            // 
+            // txtPassWordDangKi
+            // 
+            this.txtPassWordDangKi.Location = new System.Drawing.Point(215, 19);
+            this.txtPassWordDangKi.Name = "txtPassWordDangKi";
+            this.txtPassWordDangKi.Size = new System.Drawing.Size(310, 22);
+            this.txtPassWordDangKi.TabIndex = 2;
+            // 
+            // lblPassWordDangKi
+            // 
+            this.lblPassWordDangKi.AutoSize = true;
+            this.lblPassWordDangKi.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassWordDangKi.Location = new System.Drawing.Point(20, 19);
+            this.lblPassWordDangKi.Name = "lblPassWordDangKi";
+            this.lblPassWordDangKi.Size = new System.Drawing.Size(110, 24);
+            this.lblPassWordDangKi.TabIndex = 0;
+            this.lblPassWordDangKi.Text = "Mật khẩu :";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.txtRePassDangKi);
+            this.panel4.Controls.Add(this.lblRePassDangKi);
+            this.panel4.Location = new System.Drawing.Point(70, 204);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(550, 55);
+            this.panel4.TabIndex = 3;
+            // 
+            // txtRePassDangKi
+            // 
+            this.txtRePassDangKi.Location = new System.Drawing.Point(218, 19);
+            this.txtRePassDangKi.Name = "txtRePassDangKi";
+            this.txtRePassDangKi.Size = new System.Drawing.Size(310, 22);
+            this.txtRePassDangKi.TabIndex = 2;
+            // 
+            // lblRePassDangKi
+            // 
+            this.lblRePassDangKi.AutoSize = true;
+            this.lblRePassDangKi.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRePassDangKi.Location = new System.Drawing.Point(14, 19);
+            this.lblRePassDangKi.Name = "lblRePassDangKi";
+            this.lblRePassDangKi.Size = new System.Drawing.Size(192, 24);
+            this.lblRePassDangKi.TabIndex = 0;
+            this.lblRePassDangKi.Text = "Nhập lại mật khẩu :";
+            // 
+            // btnDangKi
+            // 
+            this.btnDangKi.Location = new System.Drawing.Point(397, 283);
+            this.btnDangKi.Name = "btnDangKi";
+            this.btnDangKi.Size = new System.Drawing.Size(120, 45);
+            this.btnDangKi.TabIndex = 4;
+            this.btnDangKi.Text = "Đăng kí";
+            this.btnDangKi.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(189, 283);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(122, 45);
+            this.btnThoat.TabIndex = 5;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // FormDangKi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 358);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnDangKi);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Name = "FormDangKi";
+            this.Text = "Đăng kí";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtUserNameDangKi;
+        private System.Windows.Forms.Label lblUserNameDangKi;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtDisplayNameDangKi;
+        private System.Windows.Forms.Label lblDisplayNameDangKi;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txtPassWordDangKi;
+        private System.Windows.Forms.Label lblPassWordDangKi;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox txtRePassDangKi;
+        private System.Windows.Forms.Label lblRePassDangKi;
+        private System.Windows.Forms.Button btnDangKi;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
--- QuanLyNhaHang/Login.Designer.cs
+++ QuanLyNhaHang/Login.Designer.cs
@@ -38,9 +38,12 @@
             this.panel2 = new System.Windows.Forms.Panel();
             this.txtUserName = new System.Windows.Forms.TextBox();
             this.lblUserName = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.entityCommand1 = new System.Data.Entity.Core.EntityClient.EntityCommand();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -49,29 +52,33 @@
             this.panel1.Controls.Add(this.btnLogin);
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Controls.Add(this.panel2);
-            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Location = new System.Drawing.Point(1189, 389);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(558, 171);
             this.panel1.TabIndex = 0;
             // 
             // btnExit
             // 
+            this.btnExit.BackColor = System.Drawing.Color.Black;
+            this.btnExit.ForeColor = System.Drawing.Color.White;
             this.btnExit.Location = new System.Drawing.Point(412, 125);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(105, 34);
             this.btnExit.TabIndex = 3;
             this.btnExit.Text = "Thoát";
-            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.UseVisualStyleBackColor = false;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // btnLogin
             // 
+            this.btnLogin.BackColor = System.Drawing.Color.Black;
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
             this.btnLogin.Location = new System.Drawing.Point(282, 125);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(105, 34);
             this.btnLogin.TabIndex = 2;
             this.btnLogin.Text = "Đăng nhập";
-            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.UseVisualStyleBackColor = false;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // panel3
@@ -94,7 +101,9 @@
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
+            this.lblPassword.BackColor = System.Drawing.Color.Black;
             this.lblPassword.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.ForeColor = System.Drawing.Color.White;
             this.lblPassword.Location = new System.Drawing.Point(20, 16);
             this.lblPassword.Name = "lblPassword";
             this.lblPassword.Size = new System.Drawing.Size(116, 24);
@@ -120,19 +129,42 @@
             // lblUserName
             // 
             this.lblUserName.AutoSize = true;
+            this.lblUserName.BackColor = System.Drawing.Color.Black;
             this.lblUserName.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserName.ForeColor = System.Drawing.Color.White;
             this.lblUserName.Location = new System.Drawing.Point(20, 19);
             this.lblUserName.Name = "lblUserName";
             this.lblUserName.Size = new System.Drawing.Size(165, 24);
             this.lblUserName.TabIndex = 0;
             this.lblUserName.Text = "Tên đăng nhập :";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(-18, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1787, 730);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // entityCommand1
+            // 
+            this.entityCommand1.CommandTimeout = 0;
+            this.entityCommand1.CommandTree = null;
+            this.entityCommand1.Connection = null;
+            this.entityCommand1.EnablePlanCaching = true;
+            this.entityCommand1.Transaction = null;
+            // 
             // FormLogin
             // 
             this.AcceptButton = this.btnLogin;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(560, 174);
+            this.ClientSize = new System.Drawing.Size(1771, 742);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormLogin";
@@ -144,6 +176,7 @@
             this.panel3.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -159,5 +192,7 @@
         private System.Windows.Forms.TextBox txtUserName;
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Data.Entity.Core.EntityClient.EntityCommand entityCommand1;
     }
 }
